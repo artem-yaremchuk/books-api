f14c86841c880f55aae1ab48e8e651afb7388ab7
artem-yaremchuk <artem.yaremchuk@yahoo.com>
2026-03-28 00:21:17 +0200

    fix: validate date and add indexes

--- src/book/model/book.schema.ts
+++ src/book/model/book.schema.ts
@@ -41,3 +41,6 @@ export class Book {
 export const BookSchema = SchemaFactory.createForClass(Book);
 
 BookSchema.plugin(paginate);
+
+BookSchema.index({ status: 1 });
+BookSchema.index({ publishedDate: -1 });
